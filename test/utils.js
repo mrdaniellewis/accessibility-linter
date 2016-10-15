655e92ed8850bb7ff0abc1fa4c75ff65a73fd95e
Daniel Lewis <github@mrdaniellewis.co.uk>
2016-10-15 15:19:11 +0100

    refactoring tests

--- test/utils.js
+++ test/utils.js
@@ -2,43 +2,10 @@
  * Utils to make writing tests easier
  */
 (function () {
+  // Generate a unique id
   let idCount = 0;
   window.uniqueId = () => `unique-id-${++idCount}`;
 
-  // Run a function that returns a promise that will resolve
-  // once the DOM has childlist mutations.
-  // After the then handler has been run, remove the additions
-  window.when = function when(fn) {
-    const domAdditions = [];
-    const promise = new Promise(resolve => {
-      const observer = new MutationObserver(mutations => {
-        observer.disconnect();
-        mutations.forEach(mutation => mutation.addedNodes.forEach(node => domAdditions.push(node)));
-        resolve(mutations);
-      });
-      observer.observe(document, { subtree: true, childList: true });
-      fn();
-    });
-
-    promise.then = test => Promise.prototype.then.call(promise, test)
-      .then(() => {
-        domAdditions.splice(0).forEach(el => el.remove());
-      });
-
-    return promise;
-  };
-
-  window.TestLogger = class {
-    constructor() {
-      this.clear();
-    }
-
-    error() {
-      this.errors.push(Array.from(arguments));
-    }
-
-    clear() {
-      this.errors = [];
-    }
-  };
+  // Append html to the body and return the first element
+  window.appendToBody = html => $(html).appendTo('body')[0];
 }());
