93c937c0d4fedcbe9da9b5cbb7d353cf78db8820
Daniel Lewis <github@mrdaniellewis.co.uk>
2016-11-16 22:25:19 +0000

    Ensure identically named inputs are in fieldsets
    - Remove old checkbox list test
    - Remove old radio list test
    - Fixes #26

--- spec/test-specs.js
+++ spec/test-specs.js
@@ -138,105 +138,6 @@
       },
     ],
     [
-      "fieldset/checkbox-groups-in-fieldset",
-      function() {
-        let el, el2, test, logger, linter, window, document, $, appendToBody, location;
-        const when = fn => this.when(fn, this);
-
-        before(() => {
-          ({ window, document, window: { $, appendToBody, location } } = this);
-        });
-
-        beforeEach(() => {
-          ({test, logger, linter} = this);
-        });
-
-        afterEach(() => {
-          el = el2 = test = logger = linter = null;
-        });
-
-        it('generates the expected error message', () => {
-          expect(test).toGenerateErrorMessage('All checkbox groups must be within a fieldset');
-        });
-
-        context('within a form', () => {
-          it('does not add an error if a checkbox has no name', when(() => {
-            appendToBody('<form><input type="checkbox"></form>');
-          }).then(() => {
-            expect(logger).toNotHaveEntries();
-          }));
-
-          it('does not add an error if a checkbox name is unique', when(() => {
-            appendToBody('<form><input type="checkbox" name="x"></form>');
-          }).then(() => {
-            expect(logger).toNotHaveEntries();
-          }));
-
-          it('does not add an error if a checkbox name is unique within a form', when(() => {
-            appendToBody('<form><input type="checkbox" name="x"></form>');
-            appendToBody('<form><input type="checkbox" name="x"></form>');
-          }).then(() => {
-            expect(logger).toNotHaveEntries();
-          }));
-
-          it('does not add an error if a checkbox group is in a fieldset', when(() => {
-            appendToBody('<form><fieldset><input type="checkbox" name="x"><input type="checkbox" name="x"></fieldset></form>');
-          }).then(() => {
-            expect(logger).toNotHaveEntries();
-          }));
-
-          it('adds an error if checkbox group is not in a fieldset', when(() => {
-            appendToBody('<form><input type="checkbox" name="x"><input type="checkbox" name="x"></form>');
-            el = $('input')[0];
-            el2 = $('input')[1];
-          }).then(() => {
-            expect(logger).toHaveEntries([test, el], [test, el2]);
-          }));
-        });
-
-        context('outside of a form', () => {
-          it('does not add an error if a checkbox has no name', when(() => {
-            appendToBody('<input type="checkbox">');
-          }).then(() => {
-            expect(logger).toNotHaveEntries();
-          }));
-
-          it('does not add an error if a checkbox name is unique', when(() => {
-            appendToBody('<input type="checkbox" name="x">');
-          }).then(() => {
-            expect(logger).toNotHaveEntries();
-          }));
-
-          it('does not add an error if a checkbox name is unique outside of a form', when(() => {
-            appendToBody('<input type="checkbox" name="x">');
-            appendToBody('<form><input type="checkbox" name="x"></form>');
-          }).then(() => {
-            expect(logger).toNotHaveEntries();
-          }));
-
-          it('does not add an error if a checkbox group is in a fieldset', when(() => {
-            appendToBody('<fieldset><input type="checkbox" name="x"><input type="checkbox" name="x"></fieldset>');
-          }).then(() => {
-            expect(logger).toNotHaveEntries();
-          }));
-
-          it('adds an error if checkbox group is not in a fieldset', when(() => {
-            appendToBody('<input type="checkbox" name="x"><input type="checkbox" name="x">');
-            el = $('input')[0];
-            el2 = $('input')[1];
-          }).then(() => {
-            expect(logger).toHaveEntries([test, el], [test, el2]);
-          }));
-
-          it('does not blow up if the name required escaping', when(() => {
-            appendToBody('<input type="checkbox" name="&quot; \\">');
-          }).then(() => {
-            expect(logger).toNotHaveEntries();
-          }));
-        });
-      },
-    ],
-    [
       "fieldset/fieldset-has-legend",
       function() {
         let el, el2, test, logger, linter, window, document, $, appendToBody, location;
@@ -346,7 +247,7 @@
       },
     ],
     [
-      "fieldset/radios-in-fieldset",
+      "fieldset/multiple-in-fieldset",
       function() {
         let el, el2, test, logger, linter, window, document, $, appendToBody, location;
         const when = fn => this.when(fn, this);
@@ -364,20 +265,94 @@
         });
 
         it('generates the expected error message', () => {
-          expect(this.test).toGenerateErrorMessage('All radio inputs must be within a fieldset');
+          expect(test).toGenerateErrorMessage('Multiple inputs with the same name should be in a fieldset');
         });
 
-        it('adds an error if a radio is not in a fieldset', when(() => {
-          el = appendToBody('<input type="radio">');
-        }).then(() => {
-          expect(logger).toHaveEntries([test, el]);
-        }));
+        ['input', 'textarea', 'select'].forEach((name) => {
+          context(`for <${name}>`, () => {
+            context('within a form', () => {
+              it('does not add an error if the control has no name', when(() => {
+                appendToBody(`<form><${name} /></form>`);
+              }).then(() => {
+                expect(logger).toNotHaveEntries();
+              }));
 
-        it('does not add an error if a radio is in a fieldset', when(() => {
-          appendToBody('<fieldset><input type="radio"></fieldset>');
-        }).then(() => {
-          expect(logger).toNotHaveEntries();
-        }));
+              it('does not add an error if the input is unique', when(() => {
+                appendToBody(`<form><${name} name="x" /></form>`);
+              }).then(() => {
+                expect(logger).toNotHaveEntries();
+              }));
+
+              it('does not add an error if a hidden input has the same name', when(() => {
+                appendToBody(`<form><${name} name="x" /><input type="hidden" name="x" /></form>`);
+              }).then(() => {
+                expect(logger).toNotHaveEntries();
+              }));
+
+              it('does not add an error if an input is unique within a form', when(() => {
+                appendToBody(`<form><${name} name="x" /></form>`);
+                appendToBody(`<form><${name} name="x" /></form>`);
+              }).then(() => {
+                expect(logger).toNotHaveEntries();
+              }));
+
+              it('does not add an error if the inputs are in a fieldset', when(() => {
+                appendToBody(`<form><fieldset><${name} name="x" /><${name} name="x" /></fieldset></form>`);
+              }).then(() => {
+                expect(logger).toNotHaveEntries();
+              }));
+
+              it('adds an error if the inputs are not in a fieldset', when(() => {
+                appendToBody(`<form><${name} name="x" /><${name} name="x" /></form>`);
+                el = $(name)[0];
+                el2 = $(name)[1];
+              }).then(() => {
+                expect(logger).toHaveEntries([test, el], [test, el2]);
+              }));
+            });
+
+            context('outside of a form', () => {
+              it('does not add an error if the control has no name', when(() => {
+                appendToBody(`<${name} />`);
+              }).then(() => {
+                expect(logger).toNotHaveEntries();
+              }));
+
+              it('does not add an error if the input is unique', when(() => {
+                appendToBody(`<${name} name="x" />`);
+              }).then(() => {
+                expect(logger).toNotHaveEntries();
+              }));
+
+              it('does not add an error if a hidden input has the same name', when(() => {
+                appendToBody(`<${name} name="x" /><input type="hidden" name="x" />`);
+              }).then(() => {
+                expect(logger).toNotHaveEntries();
+              }));
+
+              it('does not add an error if an input is unique outside of the form', when(() => {
+                appendToBody(`<${name} name="x" />`);
+                appendToBody(`<form><${name} name="x" /></form>`);
+              }).then(() => {
+                expect(logger).toNotHaveEntries();
+              }));
+
+              it('does not add an error if the inputs are in a fieldset', when(() => {
+                appendToBody(`<fieldset><${name} name="x" /><${name} name="x" /></fieldset>`);
+              }).then(() => {
+                expect(logger).toNotHaveEntries();
+              }));
+
+              it('adds an error if the inputs are not in a fieldset', when(() => {
+                appendToBody(`<${name} name="x" /><${name} name="x" />`);
+                el = $(name)[0];
+                el2 = $(name)[1];
+              }).then(() => {
+                expect(logger).toHaveEntries([test, el], [test, el2]);
+              }));
+            });
+          });
+        });
       },
     ],
     [
