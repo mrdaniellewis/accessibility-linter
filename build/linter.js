2328bbf9196be6c38bb4271d7a1d3bd85f6b2fd8
Daniel Lewis <github@mrdaniellewis.co.uk>
2017-02-27 20:21:58 +0000

    merge master and latest build

--- build/linter.js
+++ build/linter.js
@@ -40,7 +40,7 @@ module.exports = new Map([
 ]);
 },{"./rules/aria/attribute-values/rule.js":9,"./rules/aria/deprecated-attributes/rule.js":10,"./rules/aria/immutable-role/rule.js":11,"./rules/aria/invalid-attributes/rule.js":12,"./rules/aria/no-focusable-hidden/rule.js":13,"./rules/aria/no-none-without-presentation/rule.js":14,"./rules/aria/roles/rule.js":15,"./rules/colour-contrast/aa/rule.js":16,"./rules/colour-contrast/aaa/rule.js":17,"./rules/data-attributes/rule.js":18,"./rules/elements/obsolete/rule.js":19,"./rules/elements/unknown/rule.js":20,"./rules/fieldset/fieldset-has-legend/rule.js":21,"./rules/fieldset/legend-has-fieldset/rule.js":22,"./rules/fieldset/multiple-in-fieldset/rule.js":23,"./rules/headings/rule.js":24,"./rules/ids/imagemap-ids/rule.js":25,"./rules/ids/labels-have-inputs/rule.js":26,"./rules/ids/list-id/rule.js":27,"./rules/ids/no-duplicate-anchor-names/rule.js":28,"./rules/ids/unique-id/rule.js":29,"./rules/labels/area/rule.js":30,"./rules/labels/aria-command/rule.js":31,"./rules/labels/controls/rule.js":32,"./rules/labels/headings/rule.js":33,"./rules/labels/img/rule.js":34,"./rules/labels/links/rule.js":35,"./rules/labels/tabindex/rule.js":36,"./rules/lang/rule.js":37,"./rules/no-button-without-type/rule.js":38,"./rules/no-empty-select/rule.js":39,"./rules/no-links-to-missing-fragments/rule.js":40,"./rules/no-multiple-select/rule.js":41,"./rules/no-outside-controls/rule.js":42,"./rules/no-reset/rule.js":43,"./rules/title/rule.js":45}],"./version":[function(_dereq_,module,exports){
 "use strict";
-module.exports = "1.5.0"
+module.exports = "1.5.1"
 },{}],1:[function(_dereq_,module,exports){
 "use strict";
 /**
@@ -501,17 +501,17 @@ module.exports = {
  */
 
 const boolean = {
-  type: 'true/false',
+  type: 'token',
   tokens: ['true', 'false'],
 };
 
 const tristate = {
-  type: 'tristate',
+  type: 'token',
   tokens: ['true', 'false', 'mixed', 'undefined'],
 };
 
 const nilableBoolean = {
-  type: 'true/false/undefined',
+  type: 'token',
   tokens: ['true', 'false', 'undefined'],
 };
 
@@ -2961,7 +2961,7 @@ class AccessibleName {
 
     const element = config.elements[this.nodeName];
     if (element && element[prop]) {
-      const value = element[prop](this.el);
+      const value = element[prop](this.el, this.utils);
       if (typeof value === 'string') {
         return value;
       }
