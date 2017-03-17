6737a486e1774e45caf5dd123992daec2356f87d
Daniel Lewis <github@mrdaniellewis.co.uk>
2017-03-17 20:52:44 +0000

    opt in utils.hidden to match aria-hidden elements

--- test/specs/utils/hidden.js
+++ test/specs/utils/hidden.js
@@ -12,16 +12,6 @@ describe('#hidden', () => {
     expect(utils.hidden(el)).toEqual(false);
   });
 
-  it('is true for elements that have aria-hidden=true', () => {
-    const el = appendToBody('<div aria-hidden="true">x</div>');
-    expect(utils.hidden(el)).toEqual(true);
-  });
-
-  it('is true for elements that have a parent set to aria-hidden=true', () => {
-    const el = appendToBody('<div aria-hidden="true"><p>foo</p></div>');
-    expect(utils.hidden(utils.$('p', el))).toEqual(true);
-  });
-
   it('is true for elements that are display: none', () => {
     const el = appendToBody('<div style="display: none">foo</div>');
     expect(utils.hidden(el)).toEqual(true);
@@ -58,7 +48,7 @@ describe('#hidden', () => {
 
   it('caches the hidden status', () => {
     const el = appendToBody('<div style="display: none">foo</div>');
-    const spy = expect.spyOn(el, 'getAttribute').andCallThrough();
+    const spy = expect.spyOn(el, 'getClientRects').andCallThrough();
     utils.hidden(el);
     expect(utils.hidden(el)).toEqual(true);
     expect(spy.calls.length).toEqual(1);
@@ -71,36 +61,46 @@ describe('#hidden', () => {
     expect(utils.hidden(el2)).toEqual(false);
   });
 
-  describe('noAria option', () => {
-    it('ignores an aria-hidden element if true', () => {
+  it('is false for elements that have aria-hidden=true', () => {
+    const el = appendToBody('<div aria-hidden="true">x</div>');
+    expect(utils.hidden(el)).toEqual(false);
+  });
+
+  it('is false for elements that have a parent set to aria-hidden=true', () => {
+    const el = appendToBody('<div aria-hidden="true"><p>foo</p></div>');
+    expect(utils.hidden(el.querySelector('p'))).toEqual(false);
+  });
+
+  describe('ariaHidden option', () => {
+    it('is true if an element is aria-hidden', () => {
       const el = appendToBody('<div aria-hidden="true">foo</div>');
-      expect(utils.hidden(el, { noAria: true })).toEqual(false);
+      expect(utils.hidden(el, { ariaHidden: true })).toEqual(true);
     });
 
-    it('does not ignore a aria-hidden element if false', () => {
-      const el = appendToBody('<div aria-hidden="true">foo</div>');
-      expect(utils.hidden(el, { noAria: false })).toEqual(true);
+    it('is true if an elements parent is aria-hidden', () => {
+      const el = appendToBody('<div aria-hidden="true"><p>foo</p></div>');
+      expect(utils.hidden(el.querySelector('p'), { ariaHidden: true })).toEqual(true);
     });
 
     it('caches the hidden status', () => {
       const el = appendToBody('<div aria-hidden="true">foo</div>');
-      const spy = expect.spyOn(el, 'getClientRects').andCallThrough();
-      utils.hidden(el, { noAria: true });
-      expect(utils.hidden(el, { noAria: true })).toEqual(false);
+      const spy = expect.spyOn(el, 'getAttribute').andCallThrough();
+      utils.hidden(el, { ariaHidden: true });
+      expect(utils.hidden(el, { ariaHidden: true })).toEqual(true);
       expect(spy.calls.length).toEqual(1);
     });
 
     it('caches per element', () => {
       const el = appendToBody('<div style="display: none">foo</div>');
       const el2 = appendToBody('<div>foo</div>');
-      utils.hidden(el, { noAria: true });
+      utils.hidden(el, { ariaHidden: true });
       expect(utils.hidden(el2, { noAria: true })).toEqual(false);
     });
 
     it('caches aria and not aria separately', () => {
       const el = appendToBody('<div aria-hidden="true">foo</div>');
-      utils.hidden(el, { noAria: false });
-      expect(utils.hidden(el)).toEqual(true);
+      utils.hidden(el, { ariaHidden: true });
+      expect(utils.hidden(el)).toEqual(false);
     });
   });
 });
